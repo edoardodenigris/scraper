6f4a7ccde9b7450c0ee2735b06892b75254427f6
edoardodenigris <edoardodenigris2@gmail.com>
2021-09-25 17:44:13 +0200

    added shipping costs

--- scraper.py
+++ scraper.py
@@ -97,6 +97,24 @@ def get_expert_estimates(list_of_ids):
     return expert_df
 
 
+def get_shipping_costs(list_of_ids):
+    shipping_cost_dict = {}
+    for ids in list_of_ids:
+        try:
+            r = requests.get(
+                'https://www.catawiki.com/buyer/api/v2/lots/%s/shipping?locale=it&currency_code=EUR' % str(ids))
+            c = r.content
+            # We convert json to dictionary
+            result = json.loads(c)
+            shipping_costs = pd.DataFrame(result["shipping"]["rates"])
+            shipping_cost_dict[ids] = shipping_costs[shipping_costs['region_code'] == 'it']['price'].div(100).iloc[0]
+        except:
+            pass
+        print(ids)
+    shipping_df = pd.DataFrame.from_dict(shipping_cost_dict, orient='index').reset_index()
+    shipping_df.columns = ['id', 'shipping_cost']
+    return  shipping_df
+
 # MAIN CODE
 # Setting some parameters
 n_pages = 2
@@ -115,9 +133,16 @@ bidding_details = get_bidding_details(list_of_ids)
 # We now add the expert's estimate to the df
 expert_df = get_expert_estimates(list_of_ids)
 
+# We get shipping information
+shipping_df = get_shipping_costs(list_of_ids)
+
+
 # enrich info (i.e., merging all
-dfs = [lots, auction_details, bidding_details,expert_df]
+dfs = [lots, auction_details, bidding_details,expert_df,shipping_df]
 df_final = reduce(lambda left,right: pd.merge(left,right,on='id'), dfs)
+
+# Now we have to filter out auctions that are either closed or item is sold
+df_final = df_final[(df_final['is_closed']==False) & (df_final['is_sold']==False)]
 
 
 
